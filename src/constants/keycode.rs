989ef2eed6b8eb328f9203902949c6fa15af93c2
myyrakle <sssang97@naver.com>
2022-11-14 03:11:02 +0900

    feat: key code 구현

---
+++ src/constants/keycode.rs
@@ -0,0 +1,8 @@
+pub const LEFT: u32 = 37;
+pub const RIGHT: u32 = 39;
+pub const DOWN: u32 = 40;
+pub const Z: u32 = 90;
+pub const X: u32 = 88;
+pub const A: u32 = 65;
+pub const SPACE: u32 = 32;
+pub const SHIFT: u32 = 16;
--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,3 +1,4 @@
 pub mod character;
 pub mod color;
+pub mod keycode;
 pub mod time;
