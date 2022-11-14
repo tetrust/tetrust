bbe7bb7a1673138e121017484cac27d7aa234dbc
pikpokjeon <pikpokjeon@gmail.com>
2022-11-15 06:31:15 +0900

    STYLE Board배경 ,선 색상변경

--- src/constants/color.rs
+++ src/constants/color.rs
@@ -1,10 +1,10 @@
-pub const BOARD_DEFAULT_COLOR: &str = "#f4f3f5";
-pub const BOARD_STROKE_DEFAULT_COLOR: &str = "#000000";
+pub const BOARD_DEFAULT_COLOR: &str = "#080808";
+pub const BOARD_STROKE_DEFAULT_COLOR: &str = "#131313";
 
-pub const NEXT_DEFAULT_COLOR: &str = "#212121";
-pub const NEXT_STROKE_DEFAULT_COLOR: &str = "#000000";
+pub const NEXT_DEFAULT_COLOR: &str = "#080808";
+pub const NEXT_STROKE_DEFAULT_COLOR: &str = "#131313";
 
-pub const HOLD_DEFAULT_COLOR: &str = "#212121";
-pub const HOLD_STROKE_DEFAULT_COLOR: &str = "#000000";
+pub const HOLD_DEFAULT_COLOR: &str = "#080808";
+pub const HOLD_STROKE_DEFAULT_COLOR: &str = "#131313";
 
 pub const GHOST_DEFAULT_COLOR: &str = "#d3d3d3";
