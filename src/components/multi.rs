6f2a5e3bc84c4662a7b853b6a867f242b97ed2d6
myyrakle <sssang97@naver.com>
2023-05-01 17:58:01 +0900

    add: TODO comments

--- src/components/multi.rs
+++ src/components/multi.rs
@@ -2,6 +2,9 @@ use crate::components::game_ui;
 
 use yew::{function_component, html, Html};
 
+// TODO: 구현 필요
+// 기존 GameUI 컴포넌트를 재사용 가능하게 만들어서 양쪽에 하나씩 두는 식으로 생각중
+
 #[function_component(MultiPlay)]
 pub fn multiplay() -> Html {
     html! {
