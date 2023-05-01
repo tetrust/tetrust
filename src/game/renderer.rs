e53353430f18693cfe62ad3ff851376ec362cb97
myyrakle <sssang97@naver.com>
2023-05-02 02:59:35 +0900

    refac: event handling

--- src/game/renderer.rs
+++ src/game/renderer.rs
@@ -86,45 +86,113 @@ impl GameRenderer {
 
         log::info!("GAME START");
 
-        // gravity_drop - 중력 스레드
-        let game_info = Rc::clone(&self.game_info);
+        let _game_info = Rc::clone(&self.game_info);
+        let game_info = Rc::clone(&_game_info);
+        let event_queue = Rc::clone(&self.event_queue);
+        let _key_states = Rc::clone(&self.key_states);
+
         let mut former_lock_delay_count: u8 = 0;
 
+        // gravity_drop - 중력 스레드
         spawn_local(async move {
-            // 시작 기준점
-            let mut start_point = instant::Instant::now();
+            // 마지막으로 중력 처리한 시점
+            let mut last_tick_point = instant::Instant::now();
 
-            let game_info = game_info;
-            let _game_info = Rc::clone(&game_info);
+            let mut left_keydown_time: Option<instant::Instant> = None;
+            let mut right_keydown_time: Option<instant::Instant> = None;
 
-            // 기본 100밀리초 단위마다 반복해서 타임 체크 (더 세밀한 제어가 필요하다면 문제없는 선에서 낮춰도 무방)
             let mut future_list = IntervalStream::new(GRAVITY_DROP_INTERVAL).map(move |_| {
-                let mut game_info = game_info.borrow_mut();
-                if former_lock_delay_count != game_info.lock_delay_count {
-                    if game_info.lock_delay_count < 8 {
-                        start_point = instant::Instant::now();
+                // 락 딜레이 처리
+                if former_lock_delay_count != game_info.borrow().lock_delay_count {
+                    if game_info.borrow().lock_delay_count < 8 {
+                        last_tick_point = instant::Instant::now();
                     }
-                    former_lock_delay_count = game_info.lock_delay_count;
+                    former_lock_delay_count = game_info.borrow().lock_delay_count;
                 }
 
-                game_info.running_time = {
-                    let elapsed_time = Date::now() - game_info.start_time.get_time();
+                // 현재 실행시간 기록
+                game_info.borrow_mut().running_time = {
+                    let elapsed_time = Date::now() - game_info.borrow().start_time.get_time();
                     elapsed_time as u128
                 };
 
-                let duration = start_point.elapsed();
+                let duration = last_tick_point.elapsed();
 
                 // gravity_drop이 발생하지 않은 시점에서 경과된 시간.
                 let elapsed_time = duration.as_millis();
 
                 // 여기서 딜레이 커스텀하면 될듯
-                let delay =
-                    game_info.gravity_drop_interval as u128 + (game_info.lock_delay as u128);
+                let delay = game_info.borrow().gravity_drop_interval as u128
+                    + (game_info.borrow().lock_delay as u128);
 
                 // 지정된 딜레이만큼 지났다면 다시 초기화하고 gravity_drop 한칸 수행
                 if elapsed_time >= delay {
-                    start_point = instant::Instant::now();
-                    game_info.gravity_drop();
+                    last_tick_point = instant::Instant::now();
+                    game_info.borrow_mut().gravity_drop();
+                }
+
+                // TODO: das에 arr 적용 필요
+                // 일단은 전부 arr 0으로 가정해서 구현했는데,
+                // arr 0가 아닐때는 move_end가 아니라 arr 시간이 지날때마다 한칸씩 이동하게 해야할듯
+
+                // 키 이벤트 핸들링
+                if !event_queue.borrow().is_empty() {
+                    let event = event_queue.borrow_mut().pop_front().unwrap();
+
+                    match event {
+                        Event::LeftMove => {
+                            game_info.borrow_mut().left_move();
+                            left_keydown_time = Some(instant::Instant::now());
+                        }
+                        Event::LeftMoveStop => {
+                            left_keydown_time = None;
+                        }
+                        Event::RightMove => {
+                            game_info.borrow_mut().right_move();
+                            right_keydown_time = Some(instant::Instant::now());
+                        }
+                        Event::RightMoveStop => {
+                            right_keydown_time = None;
+                        }
+                        Event::LeftRotate => {
+                            game_info.borrow_mut().left_rotate();
+                        }
+                        Event::RightRotate => {
+                            game_info.borrow_mut().right_rotate();
+                        }
+                        Event::SoftDrop => {
+                            game_info.borrow_mut().soft_drop();
+                        }
+                        Event::HardDrop => {
+                            game_info.borrow_mut().hard_drop();
+                        }
+                        Event::Hold => {
+                            game_info.borrow_mut().hold();
+                        }
+                        Event::DoubleRotate => {
+                            game_info.borrow_mut().double_rotate();
+                        }
+                    }
+                }
+
+                // left das 처리
+                if let Some(keydown_time) = left_keydown_time {
+                    let elapsed_time = keydown_time.elapsed().as_millis();
+
+                    if elapsed_time >= game_info.borrow().das as u128 {
+                        game_info.borrow_mut().left_move_end();
+                        left_keydown_time = None;
+                    }
+                }
+
+                // right das 처리
+                if let Some(keydown_time) = right_keydown_time {
+                    let elapsed_time = keydown_time.elapsed().as_millis();
+
+                    if elapsed_time >= game_info.borrow().das as u128 {
+                        game_info.borrow_mut().right_move_end();
+                        right_keydown_time = None;
+                    }
                 }
             });
 
