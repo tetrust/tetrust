d86da9c9a9db4aeab46d998d5783f55fc36f8839
myyrakle <sssang97@naver.com>
2023-07-01 00:01:22 +0900

    impl: set_local_value

--- src/lib/js_bind/localstorage.rs
+++ src/lib/js_bind/localstorage.rs
@@ -1,7 +1,13 @@
 use super::window::window;
 
-pub fn get_local_value(id: &str) -> Option<String> {
+pub fn get_local_value(key: &str) -> Option<String> {
     let local_storage = window().local_storage().ok().flatten()?;
 
-    local_storage.get_item(id).ok().flatten()
+    local_storage.get_item(key).ok().flatten()
+}
+
+pub fn set_local_value(key: &str, value: String) {
+    let local_storage = window().local_storage().ok().flatten()?;
+
+    local_storage.set_item(key, &value);
 }
