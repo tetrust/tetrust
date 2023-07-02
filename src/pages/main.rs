4215f3bbc544b455c761de3ad18862fd71341330
andadac <andadac717@gmail.com>
2023-07-02 13:35:31 +0700

    fancy blue buttons to the main page

--- src/pages/main.rs
+++ src/pages/main.rs
@@ -5,19 +5,21 @@ use yew_router::prelude::*;
 #[function_component(MainPage)]
 pub fn main_page() -> Html {
     html! {
-        <div>
-            <h1>{ "Main Page" }</h1>
+        <div class="container w-[200px] m-[20px]">
+            <div class="flex flex-col gap-[10px]">
+                <h1 class="text-center">{"Tetrust: Main Page" }</h1>
 
-            <div>
-                <Link<Route> to={Route::SinglePlay}>{ "Single" }</Link<Route>>
-            </div>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full">
+                    <Link<Route> to={Route::SinglePlay}>{ "Single" }</Link<Route>>
+                </button>
 
-            <div>
-                <Link<Route> to={Route::MultiPlay}>{ "Multi" }</Link<Route>>
-            </div>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full">
+                    <Link<Route> to={Route::MultiPlay}>{ "Multi" }</Link<Route>>
+                </button>
 
-            <div>
-                <Link<Route> to={Route::Setting}>{ "Setting" }</Link<Route>>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full">
+                    <Link<Route> to={Route::Setting}>{ "Setting" }</Link<Route>>
+                </button>
             </div>
         </div>
     }
