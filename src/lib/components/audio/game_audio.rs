e80e463def2dd022c1a08d860ebf331fae33cda4
myyrakle <sssang97@naver.com>
2023-07-05 01:44:05 +0900

    refac: get audio element

--- src/lib/components/audio/game_audio.rs
+++ src/lib/components/audio/game_audio.rs
@@ -1,31 +1,31 @@
 use wasm_bindgen::JsCast;
+use web_sys::HtmlAudioElement;
 use yew::{function_component, html, Html};
 
 use crate::lib::js_bind::document::document;
 
-pub fn play_game_audio() {
+fn get_audio() -> Option<HtmlAudioElement> {
     let audio = document()
-        .get_element_by_id("game-audio")
-        .unwrap()
+        .get_element_by_id("game-audio")?
         .dyn_into::<web_sys::HtmlAudioElement>()
-        .map_err(|_| ())
-        .unwrap();
+        .ok()?;
 
-    if let Err(error) = audio.play() {
-        log::error!("Failed to play audio: {:?}", error);
+    Some(audio)
+}
+
+pub fn play_game_audio() {
+    if let Some(audio) = get_audio() {
+        if let Err(error) = audio.play() {
+            log::error!("Failed to play audio: {:?}", error);
+        }
     }
 }
 
 pub fn stop_game_audio() {
-    let audio = document()
-        .get_element_by_id("game-audio")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlAudioElement>()
-        .map_err(|_| ())
-        .unwrap();
-
-    if let Err(error) = audio.pause() {
-        log::error!("Failed to stop audio: {:?}", error);
+    if let Some(audio) = get_audio() {
+        if let Err(error) = audio.pause() {
+            log::error!("Failed to stop audio: {:?}", error);
+        }
     }
 }
 
