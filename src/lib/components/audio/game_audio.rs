d20e2abad36c81935b3795cb003a639b1a9c7d21
myyrakle <sssang97@naver.com>
2023-07-05 01:31:45 +0900

    impl: play_game_audio

--- src/lib/components/audio/game_audio.rs
+++ src/lib/components/audio/game_audio.rs
@@ -1,10 +1,32 @@
+use wasm_bindgen::JsCast;
 use yew::{function_component, html, Html};
+
+use crate::lib::js_bind::document::document;
+
+pub fn play_game_audio() {
+    let audio = document()
+        .get_element_by_id("game-audio")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlAudioElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    if let Err(error) = audio.play() {
+        log::error!("Failed to play audio: {:?}", error);
+    }
+}
 
 #[function_component(GameAudio)]
 pub fn game_audio() -> Html {
+    document()
+        .get_element_by_id("game-audio")
+        .unwrap()
+        .set_attribute("autoplay", "true")
+        .unwrap();
+
     html! {
         <>
-            <audio autoplay={true} loop={true}>
+            <audio id="game-audio" loop={true}>
                 <source src={"/resource/sound/tetrust.ogg"} type={"audio/mp3"}/>
             </audio>
         </>
